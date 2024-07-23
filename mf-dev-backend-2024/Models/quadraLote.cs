de3a6a4ee02bf717401dafd96ce11b29a290489a
Luiz Flávio <arthu@DESKTOP-1AIC191>
2024-07-23 13:26:35 -0300

    Configuração do Banco de Dados e tabela quadra e lote

---
+++ mf-dev-backend-2024/Models/quadraLote.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend_2024.Models
+{
+    [Table("Quadralotes")] 
+    public class  QuadraLote  
+    {
+        [Key]   
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Nome é Obrigatório")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Quadra é Obrigatório")]
+        public string Quadra { get; set; }
+
+        [Required(ErrorMessage = "Lote é Obrigatório")]
+        public string lote { get; set; }
+
+        [Required(ErrorMessage = "Email é Obrigatório")]
+            public string Email { get; set; }
+
+        [Required(ErrorMessage = "Telefone é Obrigatório")]
+        public string Telefone { get; set; }
+    }
+
+}
